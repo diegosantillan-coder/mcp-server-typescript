656fabde38a01838732b3270f27fe50860162dcf
Fernando Diego Santillan Varas <fdsantil@bancolombia.com.co>
2025-08-20 06:52:18 -0500

    feat:(mcp-server) mi primer server MCP

---
+++ src/client.ts
@@ -0,0 +1,52 @@
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+
+const transport = new StdioClientTransport({
+  command: "node",
+  args: ["server.js"]
+});
+
+const client = new Client(
+  {
+    name: "example-client",
+    version: "1.0.0"
+  }
+);
+
+await client.connect(transport);
+
+// List prompts
+const prompts = await client.listPrompts();
+
+
+
+// List resources
+const resources = await client.listResources();
+
+
+
+// list tools
+const tools = await client.listTools();
+
+
+// Read a resource
+const resource = await client.readResource({
+  uri: "file:///example.txt"
+});
+
+
+// Call a tool
+const result = await client.callTool({
+  name: "example-tool",
+  arguments: {
+    arg1: "value"
+  }
+});
+
+// call prompt
+const promptResult = await client.getPrompt({
+    name: "review-code",
+    arguments: {
+        code: "console.log(\"Hello world\")"
+    }
+})
